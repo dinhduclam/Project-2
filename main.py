9d311e0ba018e40bdbd5c444e3341faba3face13
Duc Lam <dinhduclam2001@gmail.com>
2023-06-07 00:47:57 +0700

    push

--- main.py
+++ main.py
@@ -10,6 +10,12 @@ class Status(enum.Enum):
     MEMBER = 1
     CLUSTER_HEAD = 2
 
+class Message(enum.Enum):
+    Node_Residual_Msg = 1
+    I_am_Cluster_Head = 2
+    I_am_Cluster_Member = 3
+
+
 ###########################################################
 class MyNode(wsp.Node):
     tx_range = 100
@@ -30,10 +36,13 @@ class MyNode(wsp.Node):
         self.status = Status.UNDEFINED
         self.chsv_neighbors = {}
         self.chsv_point = 1000
+
+        #for member
         self.cluster_heads = []
         self.cluster_head_official = None
+        #for cluster head
         self.members = []
-        self.cluster_neighbors = {}
+        self.cluster_adjacency = {}
 
     ###################
     def run(self):
@@ -49,15 +58,14 @@ class MyNode(wsp.Node):
     def setup_phase(self):
         self.chsv_point = self.calculate_chsv()
         yield self.timeout(BROADCAST_DELAY)
-        self.send(wsp.BROADCAST_ADDR, msg='Hello', src=self.id, chsv=self.chsv_point)
+        self.send(wsp.BROADCAST_ADDR, msg=Message.Node_Residual_Msg, src=self.id, chsv=self.chsv_point)
 
-        yield self.timeout(.5)
+        yield self.timeout(.2)
 
         if len(self.chsv_neighbors) == 0 or self.chsv_point >= max(self.chsv_neighbors.values()):
             self.switch_state(Status.CLUSTER_HEAD)
-            self.log(f'Start broadcast CH message')
             yield self.timeout(BROADCAST_DELAY)
-            self.send(wsp.BROADCAST_ADDR, msg='I am Cluster Head', src=self.id)
+            self.send(wsp.BROADCAST_ADDR, msg=Message.I_am_Cluster_Head, src=self.id)
 
         yield self.timeout(.5)
 
@@ -83,7 +91,7 @@ class MyNode(wsp.Node):
             yield self.timeout(2)
             # self.send_data_to_sink(msg='Data', src=self.id, seq=seq, data=self.data_combine)
             seq += 1
-            self.log(f"Members: {self.members}\nCluster neighbors: {self.cluster_neighbors}")
+            self.log(f"Members: {self.members}\nCluster neighbors: {self.cluster_adjacency}")
 
             # self.send_data_to_sink(msg='Cluster information', src=self.id, seq=seq, data=self.members)
         # elif self.status == Status.UNDEFINED:
@@ -93,27 +101,49 @@ class MyNode(wsp.Node):
 
     ###################
     def on_receive(self, sender, msg, src, **kwargs):
+        global SINK_NODE
 
-        if msg == 'Hello' and self.status == Status.UNDEFINED and self.id != SINK_NODE:
-            self.log(f'Receive CHSV message from {src}, chsv: {kwargs["chsv"]}')
-            self.chsv_neighbors[src] = kwargs['chsv']
+        if self.id is SINK_NODE:
+            if msg == "Data":
+                seq = kwargs['seq']
+                data = kwargs['data']
+                self.log(f"Got data from {sender} (source: {src}) - seq {seq}: {data}")
+            elif msg == 'Cluster information':
+                self.log(f'Receive from {sender}: {kwargs["data"]}')
 
-        elif msg == 'I am Cluster Head' and self.id != SINK_NODE:
-            if self.status == Status.UNDEFINED:
+        elif self.status is Status.CLUSTER_HEAD:
+            if msg == Message.I_am_Cluster_Member:
+                if self.id == kwargs['cluster_head']:
+                    self.members.append(src)
+                    self.scene.addlink(sender, self.id, "CH")
+            elif msg == "My cluster heads":
+                heads = kwargs['cluster_heads']
+                for h in heads:
+                    if h != self.id:
+                        if h in self.cluster_adjacency:
+                            self.cluster_adjacency[h].append(src)
+                        else:
+                            self.cluster_adjacency[h] = [src]
+                        self.scene.addlink(h, src, "GW")
+            elif msg == "Data":
+                seq = kwargs['seq']
+                self.data_combine += f'\nSource {src}: seq {seq}'
+
+        elif self.status is Status.MEMBER:
+            if msg == Message.I_am_Cluster_Head:
+                self.cluster_heads.append(src)
+
+        elif self.status is Status.UNDEFINED:
+            if msg == Message.Node_Residual_Msg:
+                self.chsv_neighbors[src] = kwargs['chsv']
+            elif msg == Message.I_am_Cluster_Head:
                 self.switch_state(Status.MEMBER)
                 self.cluster_heads.append(src)
                 self.cluster_head_official = src
                 yield self.timeout(BROADCAST_DELAY)
-                self.send(wsp.BROADCAST_ADDR, msg='I am Cluster Member', src=self.id, cluster_head=self.cluster_head_official)
-            elif self.status == Status.MEMBER:
-                self.cluster_heads.append(src)
-
-        elif msg == 'I am Cluster Member' and self.id != SINK_NODE:
-            if self.status == Status.CLUSTER_HEAD:
-                if self.id == kwargs['cluster_head']:
-                    self.members.append(src)
-                    self.scene.addlink(sender, self.id, "CH")
-            elif self.status == Status.UNDEFINED:
+                self.send(wsp.BROADCAST_ADDR, msg=Message.I_am_Cluster_Member, src=self.id,
+                          cluster_head=self.cluster_head_official)
+            elif msg == Message.I_am_Cluster_Member:
                 if sender in self.chsv_neighbors:
                     del self.chsv_neighbors[sender]
 
@@ -121,47 +151,33 @@ class MyNode(wsp.Node):
                     self.switch_state(Status.CLUSTER_HEAD)
                     # self.log(f'Start broadcast CH message')
                     yield self.timeout(BROADCAST_DELAY)
-                    self.send(wsp.BROADCAST_ADDR, msg='I am Cluster Head', src=self.id)
-        elif msg == "My cluster heads" and self.id != SINK_NODE:
-            if self.status == Status.CLUSTER_HEAD:
-                heads = kwargs['cluster_heads']
-                for h in heads:
-                    if h != self.id:
-                        if h in self.cluster_neighbors:
-                            self.cluster_neighbors[h].append(src)
-                        else:
-                            self.cluster_neighbors[h] = [src]
-                        self.scene.addlink(h, src, "GW")
-        elif msg == 'Data':
-            if self.status == Status.CLUSTER_HEAD:
-                seq = kwargs['seq']
-                self.data_combine += f'\nSource {src}: seq {seq}'
-            elif self.id == SINK_NODE:
-                seq = kwargs['seq']
-                data = kwargs['data']
-                self.log(f"Got data from {sender} (source: {src}) - seq {seq}: {data}")
-
-        elif msg == 'Cluster information':
-            self.log(f'Receive from {sender}: {kwargs["data"]}')
+                    self.send(wsp.BROADCAST_ADDR, msg=Message.I_am_Cluster_Head, src=self.id)
+        else:
+            pass
 
     ###################
     def switch_state(self, status: Status):
         if status == Status.CLUSTER_HEAD:
-            # self.log(f'Set status to Cluster Head')
+            # set up cluster head UI
             self.status = Status.CLUSTER_HEAD
-            self.members = []
-            self.cluster_neighbors = {}
             self.scene.nodecolor(self.id, 0, 1, 0)
             self.scene.nodewidth(self.id, 2)
             self.scene.circle(
                 self.pos[0], self.pos[1],
                 self.tx_range)
+
+            # init value
+            self.members = []
+            self.cluster_adjacency = {}
+
         elif status == Status.MEMBER:
-            # self.log(f'Set status to Member')
-            self.status = Status.MEMBER
-            self.cluster_heads = []
+            # set up cluster member UI
             self.scene.nodecolor(self.id, 1, 0, 0)
             self.scene.nodewidth(self.id, 2)
+
+            # init value
+            self.status = Status.MEMBER
+            self.cluster_heads = []
 
     ###################
     def send_data_to_sink(self, msg, src, **kwargs):
@@ -173,8 +189,23 @@ class MyNode(wsp.Node):
 
     ############################
     def calculate_chsv(self):
+        # chsv = cluster head selection value
         chsv_point = self.pin
         sum_distance = 0
+
+        for distance in self.neighbor_distance_list:
+            if distance[0] <= self.tx_range:
+                sum_distance += distance[0] ** 2
+
+        if sum_distance != 0:
+            chsv_point = self.pin/math.sqrt(sum_distance)
+
+        return chsv_point
+
+    def calculate_psv(self):
+        # psv = parent selection value
+        psv_point = self.pin
+
 
         for distance in self.neighbor_distance_list:
             if distance[0] <= self.tx_range:
@@ -197,7 +228,7 @@ sim = wsp.Simulator(
 
 # line style for member links
 sim.scene.linestyle("CH", color=(0.5, 0.5, 0), width=0.5)
-sim.scene.linestyle("GW", color=(1, 0, 1), width=2)
+sim.scene.linestyle("GW", color=(1, 0, 1), width=2, arrow='both')
 
 for x in range(6):
     for y in range(6):
@@ -206,12 +237,12 @@ for x in range(6):
         node = sim.add_node(MyNode, (px, py))
         node.max_range = 500
         node.min_range = 10
-        node.tx_range = 130
+        node.tx_range = 150
 
         node.logging = True
 
 SINK_NODE = 36
-SINK_POS = (620, 620)
+SINK_POS = (680, 680)
 
 sink_node = sim.add_node(MyNode, SINK_POS)
 sink_node.tx_range = 0
