a62f9b32fa17705682bece758a07f97eaf4144a2
Duc Lam <dinhduclam2001@gmail.com>
2023-06-15 01:50:59 +0700

    push

--- main.py
+++ main.py
@@ -11,17 +11,37 @@ class Status(enum.Enum):
     CLUSTER_HEAD = 2
 
 class Message(enum.Enum):
-    Node_Residual_Msg = 1
-    I_am_Cluster_Head = 2
-    I_am_Cluster_Member = 3
-    Cluster_Head_Residual_Msg = 4
-
+    NODE_RESIDUAL = 1
+    I_AM_CLUSTER_HEAD = 2
+    I_AM_CLUSTER_MEMBER = 3
+    CLUSTER_HEAD_RESIDUAL = 4
+    ROUTE_TO_SINK = 5
 
 ###########################################################
 class MyNode(wsp.Node):
     tx_range = 100
 
+    status = Status.UNDEFINED
+    chsv_neighbors = {}
+    psv_neighbors = {}
+    chsv_point = 0
+    psv_point = 0
+    # self.pin = 1000
+    ############
+    # for member
+    cluster_heads = []
+    cluster_head_official = None
+    # for cluster head
+    psv_neighbors = {}
+    members = []
+    cluster_adjacency = {}
+    data_combine = None
+    route_to_sink = {}
+
     ###################
+    def __init__(self, sim, id, pos):
+        super().__init__(sim, id, pos)
+
     def init(self):
         super().init()
         self.status = Status.UNDEFINED
@@ -29,8 +49,7 @@ class MyNode(wsp.Node):
         self.psv_neighbors = {}
         self.chsv_point = 1000
         self.psv_point = 1000
-        self.pin = 1000
-        self.parent = None
+        # self.pin = 1000
         ############
         # for member
         self.cluster_heads = []
@@ -39,82 +58,65 @@ class MyNode(wsp.Node):
         self.psv_neighbors = {}
         self.members = []
         self.cluster_adjacency = {}
+        self.route_to_sink = {}
 
     ###################
     def run(self):
         if self.id is SINK_NODE:
             self.scene.nodecolor(self.id, 0, 0, 0)
             self.scene.nodewidth(self.id, 2)
+            yield self.timeout(1)
         else:
             self.scene.nodecolor(self.id, 0.7, 0.7, 0.7)
             self.sim.env.process(self.setup_phase())
+            self.sim.env.run(until=0.8)
+        self.sim.env.process(self.steady_phase())
 
     ###################
     def setup_phase(self):
         self.chsv_point = self.calculate_chsv()
         yield self.timeout(BROADCAST_DELAY)
-        self.send(wsp.BROADCAST_ADDR, msg=Message.Node_Residual_Msg, src=self.id, chsv=self.chsv_point)
+        self.send(wsp.BROADCAST_ADDR, msg=Message.NODE_RESIDUAL, src=self.id, chsv=self.chsv_point)
 
         yield self.timeout(.2)
 
         if len(self.chsv_neighbors) == 0 or self.chsv_point >= max(self.chsv_neighbors.values()):
             self.switch_state(Status.CLUSTER_HEAD)
             yield self.timeout(BROADCAST_DELAY)
-            self.send(wsp.BROADCAST_ADDR, msg=Message.I_am_Cluster_Head, src=self.id)
-
-        yield self.timeout(.5)
-
-        yield self.sim.env.process(self.steady_phase())
-
+            self.send(wsp.BROADCAST_ADDR, msg=Message.I_AM_CLUSTER_HEAD, src=self.id)
 
     def steady_phase(self):
-        if self.status == Status.MEMBER:
+        if self.id is SINK_NODE:
+            for node in self.cluster_heads:
+                self.send(node, msg=Message.ROUTE_TO_SINK, src=self.id, hop_count=1, path=[SINK_NODE])
+
+        elif self.status == Status.MEMBER:
             # self.scene.clearlinks()
             seq = 0
             yield self.timeout(BROADCAST_DELAY)
             self.send(wsp.BROADCAST_ADDR, msg="My cluster heads", src=self.id, cluster_heads=self.cluster_heads)
             self.log(f"Cluster head: {self.cluster_heads}")
-            while True:
-                yield self.timeout(BROADCAST_DELAY + random.uniform(0.1, 0.7))
-                # self.log(f"Send data to {SINK_NODE} (via {self.cluster_head_official}) - seq {seq}")
-                self.send(self.cluster_head_official, msg='Data', src=self.id, seq=seq)
-                seq += 1
+            # while True:
+            #     yield self.timeout(BROADCAST_DELAY + random.uniform(0.1, 0.7))
+            #     # self.log(f"Send data to {SINK_NODE} (via {self.cluster_head_official}) - seq {seq}")
+            #     self.send(self.cluster_head_official, msg='Data', src=self.id, seq=seq)
+            #     seq += 1
 
         elif self.status == Status.CLUSTER_HEAD:
             seq = 0
             self.data_combine = ''
-            yield self.timeout(0.5)
+            yield self.timeout(1)
 
-            self.psv_point = self.calculate_psv()
-            self.psv_neighbors = {}
-
-            for node in self.cluster_adjacency.keys():
-                self.timeout(BROADCAST_DELAY)
-                self.send_to_cluster_adjacency(msg=Message.Cluster_Head_Residual_Msg, src=self.id, chdest=node, psv_point=self.psv_point)
-
-            yield self.timeout(.5)
-
-            if len(self.psv_neighbors) > 0:
-                max_psv = max(self.psv_neighbors.values())
-                if self.psv_point >= max_psv:
-                    self.parent = SINK_NODE
-                else:
-                    for id in self.psv_neighbors.keys():
-                        if self.psv_neighbors[id] == max_psv:
-                            self.parent = id
-                            break
-            else:
-                self.parent = SINK_NODE
-
+            self.log(self.route_to_sink)
 
             while True:
                 seq += 1
-                if self.parent != SINK_NODE:
+                if len(self.route_to_sink) == 0:
+                    pass
+                elif self.id < 10:
                     self.timeout(BROADCAST_DELAY + random.uniform(0.1, 0.4))
-                    self.send_to_cluster_adjacency(msg='Data_Combine', chdest=self.parent, src=self.id, seq=seq, data=self.data_combine)
-                else:
-                    yield self.timeout(BROADCAST_DELAY + .5)
-                    self.send_data_to_sink(msg="Data_Combine", src=self.id, seq=seq, data=self.data_combine)
+                    self.send_back_to_sink(msg='Data_Combine', src=self.id,
+                                           seq=seq, data=self.data_combine)
 
                 self.data_combine = ''
                 yield self.timeout(1.5)
@@ -131,9 +133,12 @@ class MyNode(wsp.Node):
                 self.log(f"Got data from {sender} - seq {seq}: {data}")
             elif msg == 'Cluster information':
                 self.log(f'Receive from {sender}: {kwargs["data"]}')
+            elif msg == Message.I_AM_CLUSTER_HEAD:
+                self.cluster_heads.append(src)
+                self.cluster_head_official = src
 
         elif self.status is Status.CLUSTER_HEAD:
-            if msg == Message.I_am_Cluster_Member:
+            if msg == Message.I_AM_CLUSTER_MEMBER:
                 if self.id == kwargs['cluster_head']:
                     self.members.append(src)
                     self.scene.addlink(sender, self.id, "CH")
@@ -146,21 +151,38 @@ class MyNode(wsp.Node):
                         else:
                             self.cluster_adjacency[h] = [src]
                         self.scene.addlink(h, src, "GW")
-            elif msg == Message.Cluster_Head_Residual_Msg:
+            elif msg == Message.CLUSTER_HEAD_RESIDUAL:
                 self.psv_neighbors[src] = kwargs["psv_point"]
             elif msg == "Data":
                 seq = kwargs['seq']
                 self.data_combine += f'\nSource {src}: seq {seq}'
             elif msg == "Data_Combine":
                 self.data_combine += f'\nData from Cluster {src}: \n{kwargs["data"]}'
-                if self.parent != SINK_NODE:
+                if len(self.route_to_sink) > 0:
                     self.timeout(BROADCAST_DELAY)
-                    self.send_to_cluster_adjacency(msg='Data_Combine', chdest=self.parent, src=self.id, data=self.data_combine)
+                    self.send_back_to_sink(msg='Data_Combine', seq=kwargs['seq'],
+                                           src=self.id, data=self.data_combine)
                     self.data_combine = ""
+
+            elif msg == Message.ROUTE_TO_SINK:
+                hop_count = kwargs["hop_count"]
+                path = kwargs["path"]
+                if self.id in path:
+                    return
+                if (src in self.route_to_sink) and (hop_count >= self.route_to_sink[src]):
+                    return
+                self.route_to_sink[src] = hop_count + 1
+                self.scene.addlink(self.id, src, "SINK")
+                path.append(self.id)
+                self.log(path)
+                for node in self.cluster_adjacency.keys():
+                    self.send_to_cluster_adjacency(msg=Message.ROUTE_TO_SINK, src=self.id,
+                                                   chdest=node, hop_count=hop_count + 1, path=path)
+
         elif self.status is Status.MEMBER:
-            if msg == Message.I_am_Cluster_Head:
+            if msg == Message.I_AM_CLUSTER_HEAD:
                 self.cluster_heads.append(src)
-            elif msg == Message.Cluster_Head_Residual_Msg:
+            elif msg == Message.CLUSTER_HEAD_RESIDUAL:
                 dest = kwargs["chdest"]
                 yield self.timeout(BROADCAST_DELAY)
                 self.send(dest, msg=msg, src=src, **kwargs)
@@ -168,26 +190,28 @@ class MyNode(wsp.Node):
                 dest = kwargs["chdest"]
                 yield self.timeout(BROADCAST_DELAY)
                 self.send(dest, msg=msg, src=src, **kwargs)
-
+            elif msg == Message.ROUTE_TO_SINK:
+                dest = kwargs["chdest"]
+                yield self.timeout(BROADCAST_DELAY)
+                self.send(dest, msg=msg, src=src, **kwargs)
         elif self.status is Status.UNDEFINED:
-            if msg == Message.Node_Residual_Msg:
+            if msg == Message.NODE_RESIDUAL:
                 self.chsv_neighbors[src] = kwargs['chsv']
-            elif msg == Message.I_am_Cluster_Head:
+            elif msg == Message.I_AM_CLUSTER_HEAD:
                 self.switch_state(Status.MEMBER)
                 self.cluster_heads.append(src)
                 self.cluster_head_official = src
                 yield self.timeout(BROADCAST_DELAY)
-                self.send(wsp.BROADCAST_ADDR, msg=Message.I_am_Cluster_Member, src=self.id,
+                self.send(wsp.BROADCAST_ADDR, msg=Message.I_AM_CLUSTER_MEMBER, src=self.id,
                           cluster_head=self.cluster_head_official)
-            elif msg == Message.I_am_Cluster_Member:
+            elif msg == Message.I_AM_CLUSTER_MEMBER:
                 if sender in self.chsv_neighbors:
                     del self.chsv_neighbors[sender]
-
                 if len(self.chsv_neighbors) == 0 or self.chsv_point >= max(self.chsv_neighbors.values()):
                     self.switch_state(Status.CLUSTER_HEAD)
                     # self.log(f'Start broadcast CH message')
                     yield self.timeout(BROADCAST_DELAY)
-                    self.send(wsp.BROADCAST_ADDR, msg=Message.I_am_Cluster_Head, src=self.id)
+                    self.send(wsp.BROADCAST_ADDR, msg=Message.I_AM_CLUSTER_HEAD, src=self.id)
         else:
             pass
 
@@ -220,8 +244,17 @@ class MyNode(wsp.Node):
             self.scene.nodecolor(self.id, 0.7, 0.7, 0.7)
             self.init()
 
-
     ###################
+    def send_back_to_sink(self, msg, src, **kwargs):
+        if len(self.route_to_sink) == 0:
+            return
+
+        parent = min(self.route_to_sink, key=self.route_to_sink.get)
+        if parent != SINK_NODE:
+            self.send_to_cluster_adjacency(msg=msg, src=src, chdest=parent, **kwargs)
+        else:
+            self.send_data_to_sink(msg=msg, src=src, **kwargs)
+
     def send_to_cluster_adjacency(self, msg, src, chdest, **kwargs):
         gw_node = self.cluster_adjacency[chdest][0]
         self.send(gw_node, msg=msg, src=src, chdest=chdest, **kwargs)
@@ -237,65 +270,57 @@ class MyNode(wsp.Node):
     def calculate_chsv(self):
         # chsv = cluster head selection value
         chsv_point = self.pin
-        sum_distance = 0
+        sum_snr = 0
 
         for distance in self.neighbor_distance_list:
             if distance[0] <= self.tx_range:
-                sum_distance += distance[0] ** 2
+                z = 7 * random.uniform(0, 1)
+                b = -50 - 37.6 * math.log10(distance[0]) + z
+                snr = b / 94
+                self.log(snr)
+                sum_snr += snr
 
-        if sum_distance != 0:
-            chsv_point = self.pin/math.sqrt(sum_distance)
+        chsv_point *= sum_snr
 
         return chsv_point
 
-    def calculate_psv(self):
-        # psv = parent selection value
-
-        cluster_adjacencies = self.cluster_adjacency.keys()
-        denominator = 0
-
-        for node in self.neighbor_distance_list:
-            if node[1].id in cluster_adjacencies:
-                denominator += node[1].pin/node[0]
-
-        if denominator == 0:
-            return self.pin
-        else:
-            return self.pin/denominator
-
     def distance(self, p0, p1):
         return math.sqrt((p0[0] - p1[0]) ** 2 + (p0[1] - p1[1]) ** 2)
+
+
 ###########################################################
 sim = wsp.Simulator(
-        until=50,
-        timescale=10,
-        visual=True,
-        terrain_size=(700, 700),
-        title="Cluster based demo")
+    until=50,
+    timescale=10,
+    visual=True,
+    terrain_size=(700, 700),
+    title="Cluster based demo")
 
 # line style for member links
 sim.scene.linestyle("CH", color=(0.5, 0.5, 0), width=0.5)
 sim.scene.linestyle("GW", color=(1, 0, 1), width=2, arrow='both')
+sim.scene.linestyle("SINK", color=(0, 1, 1), width=2, arrow='both')
 
 nodes = {}
 
 for x in range(6):
     for y in range(6):
-        px = 60 + x*100 + random.uniform(-40, 40)
-        py = 60 + y*100 + random.uniform(-40, 40)
+        px = 60 + x * 100 + random.uniform(-40, 40)
+        py = 60 + y * 100 + random.uniform(-40, 40)
         node = sim.add_node(MyNode, (px, py))
         node.max_range = 500
         node.min_range = 10
         node.tx_range = 150
+        node.pin = random.uniform(700, 1000)
         node.logging = True
 
         nodes[node.id] = node
 
 SINK_NODE = 36
-SINK_POS = (680, 680)
+SINK_POS = (610, 610)
 
 sink_node = sim.add_node(MyNode, SINK_POS)
-sink_node.tx_range = 0
+sink_node.tx_range = 150
 sink_node.logging = True
 
 sim.run()
